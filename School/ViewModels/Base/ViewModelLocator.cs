0971263dfcacbee6f4327df073f71f6403d670c4
AlejoDEV <danijandro03@outlook.com>
2021-06-24 21:13:21 -0500

    Configuración homepagey locator

---
+++ School/ViewModels/Base/ViewModelLocator.cs
@@ -0,0 +1,37 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using School.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using School.ViewModels;
+namespace School.ViewModels.Base
+{
+
+    public class ViewModelLocator
+    {
+
+        private static ViewModelLocator locator;
+        public static ViewModelLocator Locator => locator ?? (locator = new ViewModelLocator());
+
+        public ViewModelLocator()
+        {
+
+
+            var nav = new NavigationService();
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            if (!SimpleIoc.Default.IsRegistered<INavigationService>())
+            {
+                SimpleIoc.Default.Register<INavigationService, NavigationService>();
+                SimpleIoc.Default.Register<HomePageModel>();
+                nav.Configure("HomePage", typeof(HomePage));
+            }
+        }
+        private HomePageModel MasterPageVM => ServiceLocator.Current.GetInstance<HomePageModel>();
+    }       
+}
+
